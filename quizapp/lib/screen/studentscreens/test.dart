412d884277233a8f7e7bf2c13ee9b85d2eebc6c4
Turbobent <kasp728h@edu.mercantec.dk>
2024-08-12 12:47:33 +0200

    test pic

--- quizapp/lib/screen/studentscreens/test.dart
+++ quizapp/lib/screen/studentscreens/test.dart
@@ -10,7 +10,7 @@ class TestApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('test name'),
+          title: const Text('Quiz App'),
         ),
         body: const Test(),
       ),
@@ -26,27 +26,37 @@ class Test extends StatefulWidget {
 }
 
 class _TestState extends State<Test> {
-  // Hardcoded list of questions and answers
+  // Hardcoded list of questions, answers, and images
   final List<Map<String, dynamic>> questions = [
     {
       'question': 'What is Flutter?',
-      'answers': ['1: A mobile SDK', '2: A programming language', '3: A database', '4: An IDE']
+      'answers': ['1: A mobile SDK', '2: A programming language', '3: A database', '4: An IDE'],
+      'image': 'assets/randers2.jpg',
     },
     {
       'question': 'What is Dart?',
-      'answers': ['A programming language', 'A web framework', 'A mobile SDK', 'A game engine']
+      'answers': ['A programming language', 'A web framework', 'A mobile SDK', 'A game engine'],
+      'image': 'assets/john.jpg',
     },
     {
       'question': 'Explain StatefulWidget in Flutter.',
-      'answers': ['A widget with state', 'A stateless widget', 'A UI component', 'A plugin']
+      'answers': ['A widget with state', 'A stateless widget', 'A UI component', 'A plugin'],
+      'image': 'assets/mike.jpg',
     },
     {
       'question': 'What is a Widget in Flutter?',
-      'answers': ['A UI component', 'A database', 'A mobile SDK', 'A programming language']
+      'answers': ['A UI component', 'A database', 'A mobile SDK', 'A programming language'],
+      'image': 'assets/randers2.jpg',
     },
     {
       'question': 'What is the difference between hot reload and hot restart?',
-      'answers': ['Hot reload updates UI instantly', 'Hot restart restarts the app', 'Both restart the app', 'Both refresh the UI' ]
+      'answers': [
+        'Hot reload updates UI instantly',
+        'Hot restart restarts the app',
+        'Both restart the app',
+        'Both refresh the UI'
+      ],
+      'image': 'assets/randers2.jpg',
     },
   ];
 
@@ -78,68 +88,82 @@ class _TestState extends State<Test> {
 
   @override
   Widget build(BuildContext context) {
-    // Get the current question and answers
+    // Get the current question, answers, and image
     final currentQuestion = questions[currentQuestionIndex]['question'];
     final currentAnswers = questions[currentQuestionIndex]['answers'] as List<String>;
+    final currentImage = questions[currentQuestionIndex]['image'];
 
     return Padding(
       padding: const EdgeInsets.all(10.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          // Display the current question
-          Text(
-            'Question ${currentQuestionIndex + 1}/${questions.length}',
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          ),
-          const SizedBox(height: 20),
-          Text(
-            currentQuestion,
-            style: const TextStyle(fontSize: 18),
-          ),
-          const SizedBox(height: 20),
-          // Display the list of answers
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: currentAnswers.asMap().entries.map((entry) {
-              int index = entry.key;
-              String answer = entry.value;
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      selectedAnswerIndex = index; // Update the selected answer index
-                    });
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: selectedAnswerIndex == index ? Colors.yellow : null, // Change the color if selected
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            // Display the current question
+            Text(
+              'Question ${currentQuestionIndex + 1}/${questions.length}',
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              currentQuestion,
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 20),
+            // Display the image related to the current question
+            Container(
+              constraints: BoxConstraints(
+                maxHeight: 200, // Maximum height of the image
+                maxWidth: double.infinity, // Ensure it uses full width available
+              ),
+              child: Image.asset(
+                currentImage,
+                fit: BoxFit.cover, // Ensures the image covers the box without distortion
+              ),
+            ),
+            const SizedBox(height: 20),
+            // Display the list of answers
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: currentAnswers.asMap().entries.map((entry) {
+                int index = entry.key;
+                String answer = entry.value;
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        selectedAnswerIndex = index; // Update the selected answer index
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: selectedAnswerIndex == index ? Colors.yellow : null, // Change the color if selected
+                    ),
+                    child: Text(answer),
                   ),
-                  child: Text(answer),
+                );
+              }).toList(),
+            ),
+            const SizedBox(height: 40),
+            // Row for the buttons
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                // Back Button
+                ElevatedButton(
+                  onPressed: currentQuestionIndex > 0 ? _previousQuestion : null,
+                  child: const Text('Back'),
                 ),
-              );
-            }).toList(),
-          ),
-          const SizedBox(height: 40),
-          // Row for the buttons
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              // Back Button
-              ElevatedButton(
-                onPressed: currentQuestionIndex > 0 ? _previousQuestion : null,
-                child: const Text('Back'),
-              ),
-              // Next Button
-              ElevatedButton(
-                onPressed: currentQuestionIndex < questions.length - 1
-                    ? _nextQuestion
-                    : null,
-                child: const Text('Next'),
-              ),
-            ],
-          ),
-        ],
+                // Next Button
+                ElevatedButton(
+                  onPressed: currentQuestionIndex < questions.length - 1
+                      ? _nextQuestion : null,
+                  child: const Text('Next'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
